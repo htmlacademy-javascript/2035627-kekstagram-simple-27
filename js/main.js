cc5624ccc287574b53e32f38770a9768203fd445
Marharyta Overednaya <pandoraaa274@gmail.com>
2022-10-09 10:47:36 +0300

    unified style of functions

--- js/main.js
+++ js/main.js
@@ -5,11 +5,13 @@
  * @param {number} maxLength Максимальная длина
  * @returns {boolean} Подходит ли по длине
  */
-const checkCommentLength = (comment, maxLength) => comment.length < maxLength;
-checkCommentLength(180, 120);
+function checkCommentLength(comment, maxLength) {
+  comment.length < maxLength;
+}
+checkCommentLength('hello world', 140);
 
 //Функция для выбора рандомного числа из заданного диапазона
-/*
+/**
  * Возвращает случайное число в заданном диапазоне, но ещё проверяет что числа положительные и можно передовать в любом порядке и числа с плавающей точкой
  * @param {number} min Положительное число
  * @param {number} max Положительное число
@@ -19,8 +21,8 @@ function getRandomNumber(a, b) {
   if (a < 0 || b < 0) {
     return NaN;
   }
-  const min = Math.ceil(a);
-  const max = Math.floor(b);
+  const min = Math.ceil(Math.min(a, b));
+  const max = Math.floor(Math.max(a, b));
   const number = Math.random() * (max + 1 - min) + min;
   return Math.floor(number);
 }
